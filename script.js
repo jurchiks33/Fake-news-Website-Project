f8afd0289ac6552d96f39fc303e4978da5bfdab4
Juris Slisans <111147249+jurchiks33@users.noreply.github.com>
2023-09-11 11:45:53 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+// <!-- I try to link script page in a link above to the page which holds only scripts,
+// but for some reasons scripts are not working, so i add them at the bottom of the html, so they
+// let the page load and dont use resources at the start of the page -->
